5e9d8b7e042faeb3784d7394598f67bd2106aab3
Gopal Lalwani <gopallalwani09@gmail.com>
2026-08-22 23:40:32 +0530

    add tool call feature and wrap it in a loop

---
+++ src/app/config.ts
@@ -0,0 +1,8 @@
+export const HARNESS_PROMPT = `
+    You are an expert AI assistant.
+
+    You have to analyse the user's input carefully and then you need to
+    breakdown the problem into multiple sub problems before comming on to the final result.
+
+    Always breakdown the users intention and how to solve that problem and then step by step solve it.
+`
